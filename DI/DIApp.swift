f793b5ee9aad759da5844bc9151ade7f78a3f98a
Ahmed Nageh <ahmdnajkh71@gmail.com>
2026-06-14 13:14:44 +0300

    Initial Commit

---
+++ DI/DIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIApp.swift
+//  DI
+//
+//  Created by Ahmed Nageh on 14/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct DIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
